6b73fdc0b4f0ab726529d8c7ec8630c770ea6248
Godsgift Akari <giftakari@yahoo.com>
2020-12-30 15:48:43 -0800

    upload database to server

--- backend/server.js
+++ backend/server.js
@@ -17,5 +17,21 @@ app.get('/', (request, response) => {
 
 app.use('/api/products', productsRoutes);
 
+app.use((req, res, next) => {
+  const error = new Error(`Not Found -${req.originalUrl}`);
+  res.status(404);
+  next(error);
+});
+
+app.use((err, req, res, next) => {
+  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+
+  res.status(statusCode);
+  res.json({
+    message: err.message,
+    stack: process.env.NODE_ENV === 'production' ? null : err.stack,
+  });
+});
+
 const PORT = process.env.PORT || 3030;
 app.listen(PORT, () => console.log(`App is running ${PORT}`.yellow.bold));
