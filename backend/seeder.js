63c3f22db72209911ef415582efaadce3142a189
Godsgift Akari <giftakari@yahoo.com>
2020-12-29 22:41:55 -0800

    set up database

--- backend/seeder.js
+++ backend/seeder.js
@@ -1,12 +1,12 @@
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import colors from 'colors';
-import users from './seeder';
+import users from './data/user.js';
 import products from './data/products.js';
-import User from './data/models/userModel.js';
-import Order from './models/orderModel.js ';
+import User from './models/userModel.js';
+import Order from './models/orderModel.js';
 import Product from './models/productModel.js ';
-import connectDB from './congfig/db.js ';
+import connectDB from './data/congfig/db.js';
 
 dotenv.config();
 
